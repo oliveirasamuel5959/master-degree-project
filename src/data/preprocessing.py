d497f2bcd9673061ecafdd30c4767097aaf99a97
Samuel Olivera <patriciosamuel097@gmail.com>
2026-07-09 12:25:51 -0300

    Reading train and test files

---
+++ src/data/preprocessing.py
@@ -0,0 +1,29 @@
+import os
+import cv2
+from pathlib import Path
+
+def dataset_frames_labels(dataset_path: Path, rand_num: int):
+  frames = []
+  labels = []
+  
+  classes_names = os.listdir(dataset_path)
+  
+  for class_name in classes_names:
+    videos_path = dataset_path / class_name
+    video_path = list(videos_path.glob("*.mp4"))[rand_num]
+  
+    cap = cv2.VideoCapture(video_path)
+    
+    if not cap.isOpened():
+      print("Error: Could not open the video file.")
+      return
+      
+    ret, frame = cap.read()
+    
+    if not ret:
+      break
+    
+    frames.append(frame)
+    labels.append(video_path.name)
+    
+  return frames, labels
